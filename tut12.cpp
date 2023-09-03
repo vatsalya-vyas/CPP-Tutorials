28b7ff1003d2eb1ab3010e067fb1e4635d9134e5
Vatsalya Vyas <vatsalyavyas@gmail.com>
2023-09-04 00:39:05 +0530

    Tutorial-12 Code

--- tut12.cpp
+++ tut12.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main() //***What is pointer? ==> Pointer is a Datatype Which Holds The address of the other datatypes***//
+
 {
+    int a = 3;
+    int *b = &a;
+    // Or
+    // int *b;
+    // b = &a;
+
+    // & --> (Address of) the operator
+    cout << "The Address of a is : " << b << endl;
+    cout << "The Address of a is : " << &a << endl;
+
+    // * --> (Value at) Dereference operator
+    cout << "The Value at address b is : " << *b << endl;
+
+    // Pointer to Pointer --> Chain of pointers
+    int **c = &b;
+    cout << "The address of b is : " << &b << endl;
+    cout << "The address of b is : " << b << endl;
+    cout << "The value at address value_at(value_at(c)) is : " << **c << endl;
 
     return 0;
 }
