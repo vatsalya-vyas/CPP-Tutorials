1d055d0884f1e068bac6bab2ac5e267b674b0058
Vatsalya Vyas <140804211+vatsalya-vyas@users.noreply.github.com>
2023-09-28 00:17:55 +0530

    Tutorial-29 Code

--- tut29.cpp
+++ tut29.cpp
@@ -22,7 +22,7 @@ Complex ::Complex(void) // ----> This is a default constructor as it takes no pa
 {
     a = 10;
     b = 20;
-    cout << "Hello world" << endl;
+    // cout << "Hello world" << endl;
 }
 
 int main()
@@ -39,3 +39,5 @@ int main()
 // 1. It should be declared in the public section of the class.
 // 2. They are automatically invoked whenever the object is created.
 // 3. They cannot return values and do not have return types.
+// 4. It can have default arguments.
+// 5. We cannot refer to their address.
