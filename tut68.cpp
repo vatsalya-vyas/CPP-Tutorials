d6263561c0c51ae31a5b5fe7b8d042b26d166d28
Vatsalya Vyas <140804211+vatsalya-vyas@users.noreply.github.com>
2023-10-26 16:41:40 +0530

    New Code Added

---
+++ tut68.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+template <class T>
+class Vasu
+{
+public:
+    T data;
+    Vasu(T a)
+    {
+        data = a;
+    }
+    void display();
+};
+
+template <class T>
+void Vasu<T>::display()
+{
+    cout << data;
+}
+
+void func(int a)
+{
+    cout << "I am first func() " << a << endl;
+}
+
+template <class T>
+void func(T a)
+{
+    cout << "I am templatised func() " << a << endl;
+}
+
+template <class T>
+void func1(T a)
+{
+    cout << "I am templatised func() " << a << endl;
+}
+
+int main()
+{
+    // Vasu<float> h(5.7);
+    // Vasu<char> h('c');
+    // Vasu<int> h(87);
+    // cout << h.data << endl;
+    // h.display();
+
+    // func(4); // Exact match takes the highest priority
+    func1(4);
+    return 0;
+}
